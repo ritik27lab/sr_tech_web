0cb7fba738afbab07cba871e71209e36ba946588
ritik27lab <61054025+ritik27lab@users.noreply.github.com>
2023-10-23 16:35:55 +0530

    Initial commit

---
+++ src/screens/Frame/Frame.tsx
@@ -0,0 +1,279 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = (): JSX.Element => {
+  return (
+    <div className="frame">
+      <div className="div">
+        <header className="header">
+          <img className="STACKRACERS" alt="Stackracers" src="https://c.animaapp.com/TjgN3R4O/img/stackracers.png" />
+          <div className="text-wrapper">Home</div>
+          <img
+            className="stacl-racer-logo"
+            alt="Stacl racer logo"
+            src="https://c.animaapp.com/TjgN3R4O/img/stacl-racer-logo-1.png"
+          />
+          <div className="who-we-are">Who We Are</div>
+          <div className="text-wrapper-2">Services</div>
+          <div className="text-wrapper-3">Reviews</div>
+          <div className="text-wrapper-4">Contact Us</div>
+          <img className="vector" alt="Vector" src="https://c.animaapp.com/TjgN3R4O/img/vector-5@2x.png" />
+          <img className="line" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-1@2x.png" />
+          <div className="overlap">
+            <img className="img" alt="Vector" src="https://c.animaapp.com/TjgN3R4O/img/vector-3@2x.png" />
+          </div>
+          <img className="vector-2" alt="Vector" src="https://c.animaapp.com/TjgN3R4O/img/vector-2@2x.png" />
+          <div className="overlap-group">
+            <img className="vector-3" alt="Vector" src="https://c.animaapp.com/TjgN3R4O/img/vector-1@2x.png" />
+            <img className="vector-4" alt="Vector" src="https://c.animaapp.com/TjgN3R4O/img/vector@2x.png" />
+          </div>
+          <div className="navbar-logo-left">
+            <div className="navbar-container">
+              <div className="navbar-content">
+                <img
+                  className="navbar-brand"
+                  alt="Navbar brand"
+                  src="https://c.animaapp.com/TjgN3R4O/img/navbar-brand.png"
+                />
+                <div className="navbar-menu">
+                  <div className="navbar-link">
+                    <div className="text-wrapper-5">About</div>
+                  </div>
+                  <div className="navbar-link">
+                    <div className="text-wrapper-5">Features</div>
+                  </div>
+                  <div className="navbar-link">
+                    <div className="text-wrapper-5">Pricing</div>
+                  </div>
+                  <button className="navbar-button">
+                    <div className="text-wrapper-6">GET STARTED</div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <div className="HOME">
+          <div className="div-2">
+            <p className="we-create-attractive">
+              <span className="span">We create attractive, functional and simple websites that get </span>
+              <span className="text-wrapper-7">results</span>
+              <span className="span">
+                .<br />
+              </span>
+            </p>
+            <p className="we-are-an-family-of">
+              We are an family of developers specialized in the creation of 360° websites, we help companies to increase
+              their online visibility, to communicate
+              <br />
+              effective and acquire future customers.
+            </p>
+          </div>
+        </div>
+        <div className="div-wrapper">
+          <div className="text-wrapper-8">Start your project journey</div>
+        </div>
+        <div className="group">
+          <div className="overlap-2">
+            <img className="rectangle" alt="Rectangle" src="https://c.animaapp.com/TjgN3R4O/img/rectangle-8-1.png" />
+            <div className="text-wrapper-9">Ux/Ui Design</div>
+            <div className="rectangle-2" />
+            <div className="text-wrapper-10">Application Development</div>
+            <div className="text-wrapper-11">Copywriting</div>
+            <div className="rectangle-3" />
+            <div className="text-wrapper-12">SEO</div>
+            <div className="rectangle-4" />
+            <div className="rectangle-5" />
+            <div className="text-wrapper-13">Blockchain Development</div>
+            <img
+              className="rectangle-6"
+              alt="Rectangle"
+              src="https://c.animaapp.com/TjgN3R4O/img/rectangle-16@2x.png"
+            />
+            <div className="text-wrapper-14">Ux/Ui Design</div>
+            <div className="text-wrapper-15">Web Dev</div>
+            <div className="text-wrapper-16">Ecommerce</div>
+            <div className="text-wrapper-17">Analytics</div>
+            <div className="text-wrapper-18">Enterprise</div>
+            <div className="rectangle-7" />
+            <div className="rectangle-8" />
+            <div className="rectangle-9" />
+            <div className="rectangle-10" />
+            <div className="text-wrapper-19">Google Maps Business</div>
+            <div className="rectangle-11" />
+          </div>
+        </div>
+      </div>
+      <div className="SERVICES">
+        <div className="service-container">
+          <p className="p">
+            We create innovative digital experiences. We work on user experience, which will be the fundamental pillar
+            of our design process.
+          </p>
+          <img className="line-2" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-3-1.png" />
+          <div className="web-dev">WEB DEV</div>
+          <div className="text-wrapper-20">UI/UX DESIGN</div>
+          <img className="line-3" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-2.png" />
+          <div className="arrow-up-right-wrapper">
+            <img
+              className="arrow-up-right"
+              alt="Arrow up right"
+              src="https://c.animaapp.com/TjgN3R4O/img/arrow-up-right-3@2x.png"
+            />
+          </div>
+          <div className="img-wrapper">
+            <img
+              className="arrow-up-right-2"
+              alt="Arrow up right"
+              src="https://c.animaapp.com/TjgN3R4O/img/arrow-up-right-2@2x.png"
+            />
+          </div>
+          <p className="we-are-experts-in">
+            <span className="text-wrapper-21">
+              We are experts in translating technical concepts and user interface into a language that is easy for your
+              audience to understand i.e. a Mobile App!
+            </span>
+            <span className="text-wrapper-22">✨</span>
+            <span className="text-wrapper-21">
+              &nbsp;&nbsp; <br />
+            </span>
+          </p>
+          <div className="app-dev">APP DEV</div>
+          <div className="overlap-group-2">
+            <img
+              className="arrow-up-right-2"
+              alt="Arrow up right"
+              src="https://c.animaapp.com/TjgN3R4O/img/arrow-up-right-2@2x.png"
+            />
+          </div>
+          <img className="line-4" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-3-1.png" />
+          <p className="a-website-isn-t">
+            A website isn&#39;t complete if you can&#39;t find it on Google. We will optimize the site in order to rank
+            as high as possible
+          </p>
+          <div className="MARKETING-SEO">MARKETING &amp; SEO</div>
+          <div className="overlap-3">
+            <img
+              className="arrow-up-right-2"
+              alt="Arrow up right"
+              src="https://c.animaapp.com/TjgN3R4O/img/arrow-up-right-2@2x.png"
+            />
+          </div>
+          <img className="line-5" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-3-1.png" />
+          <p className="text-wrapper-23">
+            Regardless of your industry, our team is ready to custom design a website that fully meets your criteria.
+          </p>
+        </div>
+        <div className="overlap-4">
+          <p className="SERVICES-WE-OFFER">
+            <span className="span">SERVICES </span>
+            <span className="text-wrapper-7">WE&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            <span className="span"> OFFER</span>
+          </p>
+          <img className="heart" alt="Heart" src="https://c.animaapp.com/TjgN3R4O/img/heart@2x.png" />
+        </div>
+      </div>
+      <div className="latest-projects">
+        <div className="text-wrapper-24">OUR LATEST PROJECTS</div>
+        <img className="rectangle-12" alt="Rectangle" src="https://c.animaapp.com/TjgN3R4O/img/rectangle-36@2x.png" />
+        <img className="rectangle-13" alt="Rectangle" src="https://c.animaapp.com/TjgN3R4O/img/rectangle-37@2x.png" />
+        <img className="rectangle-14" alt="Rectangle" src="https://c.animaapp.com/TjgN3R4O/img/rectangle-38@2x.png" />
+        <div className="text-wrapper-25">HOTPOD YOGA</div>
+        <div className="text-wrapper-26">WILD SOUL</div>
+        <div className="text-wrapper-27">SALERNO’S</div>
+        <p className="text-wrapper-28">
+          App Yoga progettato da un design basato su UX guidato dalle sue funzionalità principali.
+        </p>
+        <p className="text-wrapper-29">
+          Ecommerce basato sulla vendita di prodotti biologici con un deign accattivante e funzionale.
+        </p>
+        <p className="text-wrapper-30">
+          Progettazione grafica del ristorante Salerno’s con un stile minimal e con servizio delivery.
+        </p>
+      </div>
+      <div className="key-values">
+        <div className="overlap-5">
+          <div className="text-wrapper-31">Collaboration</div>
+          <div className="rectangle-15" />
+          <div className="text-wrapper-32">Transparency</div>
+          <div className="text-wrapper-33">United</div>
+          <div className="rectangle-16" />
+          <div className="rectangle-17" />
+          <div className="overlap-group-3">
+            <div className="time-saving">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Time Saving</div>
+            <div className="rectangle-18" />
+          </div>
+          <div className="text-wrapper-34">Strategic</div>
+          <div className="rectangle-19" />
+          <div className="text-wrapper-35">Creative</div>
+        </div>
+        <div className="rectangle-20" />
+      </div>
+      <div className="testimonials">
+        <div className="overlap-6">
+          <p className="text-wrapper-36">WHAT THEY SAY ABOUT US</p>
+          <div className="rectangle-21" />
+          <div className="rectangle-22" />
+          <img className="ellipse" alt="Ellipse" src="https://c.animaapp.com/TjgN3R4O/img/ellipse-10@2x.png" />
+          <p className="text-wrapper-37">I decided to stick on the only agency XYZ</p>
+          <div className="bionutrimed">BIONUTRIMED</div>
+          <img className="ellipse-2" alt="Ellipse" src="https://c.animaapp.com/TjgN3R4O/img/ellipse-11-1@2x.png" />
+          <div className="la-bottega-verde">LA BOTTEGA VERDE</div>
+          <div className="rectangle-23" />
+          <img className="ellipse-3" alt="Ellipse" src="https://c.animaapp.com/TjgN3R4O/img/ellipse-11@2x.png" />
+          <p className="great-job-i-met">
+            Great job, I met professionals who were able to advise me in the best way.
+            <br />
+            Clear and precise communication.
+            <br />
+            Advised.
+          </p>
+          <div className="ETNA-SICILY-TOURING">ETNA SICILY&nbsp;&nbsp;TOURING</div>
+          <p className="we-fully-relied-on">
+            we fully relied on them for the restyling of our site and we found maximum availability and excellent
+            professionalism.
+          </p>
+        </div>
+        <div className="arrow-left-wrapper">
+          <img className="arrow-left" alt="Arrow left" src="https://c.animaapp.com/TjgN3R4O/img/arrow-left@2x.png" />
+        </div>
+        <div className="arrow-right-wrapper">
+          <img className="arrow-right" alt="Arrow right" src="https://c.animaapp.com/TjgN3R4O/img/arrow-right@2x.png" />
+        </div>
+      </div>
+      <div className="contact-footer">
+        <div className="overlap-7">
+          <div className="let-s-discuss-your">LET&#39;S DISCUSS YOUR IDEA</div>
+          <img className="light-bulb" alt="Light bulb" src="https://c.animaapp.com/TjgN3R4O/img/light-bulb-1@2x.png" />
+        </div>
+        <p className="text-wrapper-38">Consult with one of our professionals to answer your questions.</p>
+        <div className="text-wrapper-39">Your Name</div>
+        <div className="text-wrapper-40">Contact No.</div>
+        <div className="text-wrapper-41">E-Mail</div>
+        <div className="text-wrapper-42">Describe your project</div>
+        <img className="line-6" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-3@2x.png" />
+        <img className="line-7" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-4@2x.png" />
+        <img className="line-8" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-5@2x.png" />
+        <img className="line-9" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-6-1@2x.png" />
+        <div className="overlap-8">
+          <div className="text-wrapper-43">SEND</div>
+          <img
+            className="paper-plane"
+            alt="Paper plane"
+            src="https://c.animaapp.com/TjgN3R4O/img/paper-plane-1@2x.png"
+          />
+        </div>
+      </div>
+      <footer className="footer">
+        <div className="overlap-9">
+          <div className="line-wrapper">
+            <img className="line-10" alt="Line" src="https://c.animaapp.com/TjgN3R4O/img/line-6.png" />
+          </div>
+          <p className="text-wrapper-44">Copyright © 2023. All rights reserved.</p>
+          <div className="privacy-police">Privacy &amp; Police</div>
+          <div className="text-wrapper-45">digitalbrox@gmail.com</div>
+        </div>
+      </footer>
+    </div>
+  );
+};
